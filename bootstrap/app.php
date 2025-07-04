8b7946f03efdfc46360440d205db0a6429b54668
KhenPlay <kenzahem@gmail.com>
2025-07-04 21:15:38 +0800

    added middleware - verifyRole

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\verifyrole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'verifyrole' => verifyRole::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
