a5ec9f4465121d57515a2bf87f41cbe99e079f1b
KhenPlay <kenzahem@gmail.com>
2025-07-04 21:36:05 +0800

    added route middleware

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,11 @@ use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
 //DASHBOARD / BACKEND
-
-Route::get('/', Main::class);
-Route::get('/users', Index::class);
-Route::get('/users/create', Create::class);
+Route::middleware('verifyRole', 'auth')->group(function(){
+    Route::get('/', Main::class);
+    Route::get('/users', Index::class);
+    Route::get('/users/create', Create::class);
+});
 
 
 //AUTH
