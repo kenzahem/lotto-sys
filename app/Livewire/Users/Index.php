0bfbc0c939d71b7ff24d5a14fc9527e3ec6e7b69
KhenPlay <kenzahem@gmail.com>
2025-07-04 22:58:31 +0800

    added lucide icons on dependences

--- app/Livewire/Users/Index.php
+++ app/Livewire/Users/Index.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\User;
+use Mary\Traits\Toast;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use Toast;
+
+    public function headers()
+    {
+        return [
+            ['key' => 'name', 'label' => 'Full Name'],
+            ['key' => 'email', 'label' => 'Email Address']
+        ];
+    }
+
+    public function users()
+    {
+        return collect(User::class);
+    }
+
     public function render()
     {
-        return view('livewire.users.index');
+        return view('livewire.users.index', [
+            'headers' => $this->headers(),
+            'users'   => $this->users()
+        ]);
     }
 }
--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-header>
+        <x-slot:middle class="!justify-end">
+            <x-button wire:navigate link="/users/create" icon="o-circle-plus" label="Create User" class="btn-primary" />
+        </x-slot:middle>
+    </x-header>
+    <x-card>
+        <x-table :headers="$headers" :rows="$users" striped @row-click="alert($event.detail.name)" />
+    </x-card>
 </div>
